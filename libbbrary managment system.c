9be84a95913bd0a3088c097189a2dfd761476d9c
Abdirahman Rage Ahmed <87867449+Abdirahman-rage@users.noreply.github.com>
2021-08-02 14:44:13 +0300

    Add files via upload
    navigation menu
    validation, the way the system can validate

--- libbbrary managment system.c
+++ libbbrary managment system.c
@@ -45,3 +45,66 @@ int main()
         if (action < 1 || action > 3)
             printf("Invalid action. Try again\n");
     } while(action < 1 || action > 3);
+    int action;
+    while(1) {
+        action = menu();
+        execute_action(action);
+        printf("Press any key to continue");
+        getchar();
+        getch();
+        system("cls");
+    }
+
+    return 0;
+}
+
+
+
+void execute_action(int action) {
+    switch (action) {
+    case 1:
+        add_user();
+        break;
+    case 2:
+        view_users();
+        break;
+    case 3:
+        printf("adding book\n");
+        break;
+    case 4:
+        close();
+        break;
+    default:
+        printf("Sorry. I don't know how to execute that\n");
+    }
+
+}
+
+void close() {
+    printf("Thank for you using the system.");
+    Sleep(5000);
+    exit(0);
+}
+
+int menu() {
+    int action;
+
+    do {
+        printf("\tCommunity Library System\n");
+        printf("Welcome Mr.Rage\n");
+        printf("1. Add user\n");
+        printf("2. View all users\n");
+        printf("3. Add book\n");
+        printf("4. Exit\n");
+        printf("Action(1-4): ");
+        scanf("%d",&action);
+        // validate
+        if (action < 1 || action > 4) {
+            printf("Invalid action.Try again\n");
+            Sleep(2000);
+            system("cls");
+        }
+    } while(action < 1 || action > 4);
+
+    return action;
+}
